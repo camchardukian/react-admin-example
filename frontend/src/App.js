1b3aaef25eb463419ab4881ede92347953d70b5d
Cameron Chardukian <camchardukian@gmail.com>
2020-10-11 18:21:52 +0700

    implemented create and edit functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import { Admin, Resource } from "react-admin";
 import restProvider from "ra-data-simple-rest";
 import StudentList from "./components/StudentList";
+import StudentCreate from "./components/StudentCreate";
+import StudentEdit from "./components/StudentEdit";
 
 import "./App.css";
 
 function App() {
   return (
     <Admin dataProvider={restProvider("http://localhost:3000")}>
-      <Resource name="students" list={StudentList} />
+      <Resource
+        name="students"
+        list={StudentList}
+        create={StudentCreate}
+        edit={StudentEdit}
+      />
     </Admin>
   );
 }
--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -15,18 +15,14 @@ const StudentList = props => {
       <Datagrid>
         <TextField source="id" />
         <TextField source="name" />
+        <TextField source="strengths" />
+        <TextField source="weaknesses" />
         <DateField source="enrolledOn" />
         <EditButton basePath="/students" />
         <DeleteButton basePath="/students" />
-        {/* <TextField source="id" />
-    <TextField source="id" /> */}
       </Datagrid>
     </List>
   );
 };
 
 export default StudentList;
-// "name": "Tina",
-// "strengths": ["grammar", "work ethic"],
-// "weaknesses": ["listening skills"],
-// "enrolledOn": "21-10-2016"
